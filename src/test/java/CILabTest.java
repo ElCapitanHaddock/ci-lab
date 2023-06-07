28b578ad08aacb69b234012c7006f522480e8784
ElCapitanHaddock <ElCapitanHaddock@users.noreply.github.com>
2023-06-07 12:45:02 -0700

    updated imports

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -1,9 +1,9 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CILabTest {
 
